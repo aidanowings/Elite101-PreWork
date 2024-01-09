5adaa3b711f82e301ec567857dfb4080f4555e0b
aidanowings <155936787+aidanowings@users.noreply.github.com>
2024-01-09 03:57:13 +0000

    Added the age function of user information
    Added if, elif, else statements regarding the user's age. Depending on the user's age, a different set of dialogue will be outputted.

--- main.py
+++ main.py
@@ -5,8 +5,22 @@ print("Welcome to the Elite 101 Chatbot!")
 # User information collection
 
 name = input("Please enter your name: ")
-age = input(f"Nice to meet you, {name}! How old are you? ")
 
+age = int(input(f"Nice to meet you, {name}! How old are you? "))
+
+if age < 15:
+  print(f"Welcome {name}! Oh it would be great to be back in my youth! How can I help you today?")
+elif age < 18:
+  print(f"Welcome {name}! Hey you got your whole life ahead of you! How can I help you today?")
+elif age < 55:
+  print(f"Welcome {name}! Ah! The thrills of adulthood! How can I help you today?")
+elif age < 120:
+  print(f"Welcome {name}! Wow! Quite the span of experiences! How can I help you today?")
+elif age > 120:
+  print(f"Welcome {name}! Wow! Breaking records with that age! How can I help you today?")
+else:
+  print("Sorry, try again.")
 # Conversation menu
+
 
 
