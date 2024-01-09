f2aa9caf4454e89cca76e9abfbe8338a2032d770
aidanowings <155936787+aidanowings@users.noreply.github.com>
2024-01-09 03:33:43 +0000

    Added the welcome message and first stages of user information
    Added a welcome message and inputs for the user's name and age.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("In Development :) Hopefully this will be done soon!")
+# Welcome message
+
+print("Welcome to the Elite 101 Chatbot!")
+
+# User information collection
+
+name = input("Please enter your name: ")
+age = input(f"Nice to meet you, {name}! How old are you? ")
+
+# Conversation menu
+
+
